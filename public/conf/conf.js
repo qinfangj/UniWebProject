9121e02704c89caa7038f2ccd77f6a122595bfea
jdelafon <julien.delafontaine@sib.swiss>
2016-10-17 13:40:25 +0200

    Pull conf and images out of gitignore

---
+++ public/conf/conf.js
@@ -0,0 +1,4 @@
+
+window.ENV = {
+    BACKEND_URL: "http://localhost:9000",
+};
--- webpack.config.js
+++ webpack.config.js
@@ -35,11 +35,14 @@ const config = {
   entry: [
     /* Bootstrap */
     'bootstrap-loader',
+
     /* Attempt to make it work with Bootstrap 4 by specifying a config file */
     //'bootstrap-loader/lib/bootstrap.loader?extractStyles&configFilePath='+__dirname+'/.bootstraprc!bootstrap-loader/no-op.js',
+
     /* Material Design Lite (https://getmdl.io) */
     /*'!!style!css!react-mdl/extra/material.min.css',*/
     /*'react-mdl/extra/material.min.js',*/
+
     /* The main entry point of your JavaScript application */
     './main.js',
   ],
