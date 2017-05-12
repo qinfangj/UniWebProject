46839df6c97536aa52b3d0f4e1a9f599af47d0e1
jdelafon <julien.delafontaine@sib.swiss>
2017-05-12 14:31:42 +0200

    Fixed test

--- test/RunsForm.spec.js
+++ test/RunsForm.spec.js
@@ -1,12 +1,18 @@
 "use strict";
 import React from 'react';
 import {shallow, mount, render} from 'enzyme';
+import store from '../core/store';
+import { Provider } from 'react-redux';
 
 import {expect} from 'chai';
 import RunsInsertForm from '../components/forms/facilityData/Runs/RunsInsertFormRedux';
 
 
-const wrapper1 = shallow(<RunsInsertForm/>);
+const wrapper1 = shallow(
+    <Provider store={store}>
+        <RunsInsertForm/>
+    </Provider>
+);
 describe('(Component) RunsInsertForm =>', () => {
 
     it('renders without exploding', () => {
