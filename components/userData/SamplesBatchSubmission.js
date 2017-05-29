bd6aaa72e2399d90327b5316638c58e023f63713
jdelafon <julien.delafontaine@sib.swiss>
2017-05-29 10:26:54 +0200

    split code

--- components/userData/SamplesBatchSubmission.js
+++ components/userData/SamplesBatchSubmission.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import css from './styles.css';
 import { connect } from 'react-redux';
-import { Form, Control } from 'react-redux-form';
 import { bindActionCreators } from 'redux';
 import {
     requestAllProjects,
@@ -15,7 +14,8 @@ import {
     requestLibAdapters,
     } from '../actions/actionCreators/optionsActionCreators';
 import batchSubmissionModel from './model';
-import inputTypes from '../forms/inputTypes';
+import * as helpers from './helpers';
+import { Form } from 'react-redux-form';
 
 
 
@@ -43,72 +43,16 @@ class SamplesBatchSubmission extends React.PureComponent {
     }
 
     /**
-     * Build a list of <option>s based on an array representing the different options for a select input.
-     * @param options: array [[id, label], ...] to become <option value={id}>{label}</option>.
-     * @returns {Array}
-     */
-    makeOptions(options) {
-        return options ? options.map((v,i) => <option value={v[0]} key={i}>{v[1]}</option>) : [];
-    }
-
-    /**
-     * Make an array of inputs of the correct type, based on the form model.
-     * @param k: the row index, to link an input to its row.
-     * @returns {Array}
-     */
-    makeInputs(k) {
-        let inputs = [];
-        for (let field of Object.keys(batchSubmissionModel)) {
-            let model = batchSubmissionModel[field];
-            let modelName = `${this.model}[${k}].${field}`;
-            let input;
-            let props = {
-                updateOn: "change",
-            };
-            switch(model.inputType) {
-                case inputTypes.TEXT:
-                    input = <Control.text model={modelName} {...props} />; break;
-                case inputTypes.NUMBER:
-                    input = <Control.input model={modelName} type="number" {...props} />; break;
-                case inputTypes.DATE:
-                    input = <Control.input model={modelName} type="date" {...props} />; break;
-                case inputTypes.CHECKBOX:
-                    input = <Control.checkbox model={modelName} />; break;
-                case inputTypes.DROPDOWN:
-                    input = (
-                        <Control.select model={modelName} {...props}>
-                            {this.makeOptions(this.props.options[model.optionsKey])}
-                        </Control.select>
-                    ); break;
-                default:
-                    break;
-            }
-            inputs.push(input);
-        }
-        return inputs;
-    }
-
-    /**
-     * Build one of the rows, given an array of <input>s.
-     * @param inputs: an array of inputs.
-     * @param k: The row index, to have a unique key for React.
-     */
-    makeRow(inputs, k) {
-        let cells = inputs.map((input,i) =>
-            <td className={css.cell} key={i}>{input}</td>
-        );
-        return <tr key={k}>{cells}</tr>;
-    }
-
-    /**
      * Build an array of rows, one for each sample from the store.
      * @returns {Array}
      */
     makeRows() {
         let rows = this.props.formData.map((sample, k) => {
-            let inputs = this.makeInputs(k);
-            let row = this.makeRow(inputs, k);
-            return row;
+            let inputs = helpers.makeInputs(this.model, this.props.options, this.modelName, k);
+            let cells = inputs.map((input,i) =>
+                <td className={css.cell} key={i}>{input}</td>
+            );
+            return <tr key={k}>{cells}</tr>;
         });
         return rows;
     }
@@ -157,8 +101,8 @@ function mapStateToProps(state) {
     }
     let formData = state.userData.samples;
     return {
-        formData: formData,
-        options: options,
+        formData: formData || [],
+        options: options || {},
     };
 }
 
---
+++ components/userData/helpers.js
@@ -0,0 +1,55 @@
+"use strict";
+import React from 'react';
+import inputTypes from '../forms/inputTypes';
+import { Control } from 'react-redux-form';
+
+
+/**
+ * Build a list of <option>s based on an array representing the different options for a select input.
+ * @param options: array [[id, label], ...] to become <option value={id}>{label}</option>.
+ * @returns {Array}
+ */
+function makeOptions(options) {
+    return options ? options.map((v,i) => <option value={v[0]} key={i}>{v[1]}</option>) : [];
+}
+
+
+/**
+ * Make an array of inputs of the correct type, based on the form model.
+ * @param model: RRF model of a row.
+ * @param modelName: RRF form model name, to connect inputs.
+ * @param k: the row index, to link an input to its row.
+ * @returns {Array}
+ */
+export function makeInputs(rowModel, options, formModelName, k) {
+    let inputs = [];
+    for (let field of Object.keys(rowModel)) {
+        let model = rowModel[field];
+        let modelName = `${formModelName}[${k}].${field}`;
+        let input;
+        let props = {
+            updateOn: "change",
+        };
+        switch(model.inputType) {
+            case inputTypes.TEXT:
+                input = <Control.text model={modelName} {...props} />; break;
+            case inputTypes.NUMBER:
+                input = <Control.input model={modelName} type="number" {...props} />; break;
+            case inputTypes.DATE:
+                input = <Control.input model={modelName} type="date" {...props} />; break;
+            case inputTypes.CHECKBOX:
+                input = <Control.checkbox model={modelName} />; break;
+            case inputTypes.DROPDOWN:
+                input = (
+                    <Control.select model={modelName} {...props}>
+                        {makeOptions(options[model.optionsKey])}
+                    </Control.select>
+                ); break;
+            default:
+                break;
+        }
+        inputs.push(input);
+    }
+    return inputs;
+}
+
