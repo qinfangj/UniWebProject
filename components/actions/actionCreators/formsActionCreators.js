ec0d13153bfe3c295c36f4e2968c51912fa1f666
jdelafon <julien.delafontaine@sib.swiss>
2017-06-26 15:56:11 +0200

    cleanup

--- components/actions/actionCreators/formsActionCreators.js
+++ components/actions/actionCreators/formsActionCreators.js
@@ -4,18 +4,6 @@ import RestService from '../../../utils/RestService';
 import { assertStoreKey, asyncAction } from './base';
 
 
-export function changeFormValue(form, field, value, valid) {
-    return (dispatch) => {
-        return dispatch({
-            type: types.forms.CHANGE_FORM_VALUE,
-            form: form,
-            field: field,
-            value: value,
-            valid: valid,
-        });
-    };
-}
-
 /* Select options. */
 
 export function getOptionsListAsync(tableName, storeKey) {
@@ -36,3 +24,16 @@ export function getSecondaryOptionsListAsync(tableName, id, storeKey) {
     return asyncAction(types.forms.GET_SECONDARY_OPTIONS_LIST, RestService.getSecondaryOptionsList.bind(null, tableName, id), args)
 }
 
+
+// Used only in MultipleSelect, relic for QueryProjects to refactor.
+export function changeFormValue(form, field, value, valid) {
+    return (dispatch) => {
+        return dispatch({
+            type: types.forms.CHANGE_FORM_VALUE,
+            form: form,
+            field: field,
+            value: value,
+            valid: valid,
+        });
+    };
+}
