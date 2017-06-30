476a8b4f24a3ee54ea6a282fcebc1d1988866c19
jdelafon <julien.delafontaine@sib.swiss>
2017-06-30 10:50:57 +0200

    forgot to add file

---
+++ components/tables/columnFormatters.js
@@ -0,0 +1,17 @@
+"use strict";
+import tableNames from './tableNames';
+
+
+
+const columnFormatters = {
+    [tableNames.PEOPLE]: (data) => data.map((d) => {
+        console.log(d)
+        d.name = d.last_name +' '+d.first_name;
+        return d;
+    }),
+
+};
+
+
+export default columnFormatters;
+
